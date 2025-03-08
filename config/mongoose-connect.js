1fcebf12938833a1f36f3a7b728e79e160b95d6a
Suman Dey <140014618+sumandey2023@users.noreply.github.com>
2025-03-08 22:41:42 +0530

    final update

--- config/mongoose-connect.js
+++ config/mongoose-connect.js
@@ -1,10 +1,24 @@
 const mongoose = require("mongoose");
 const config = require("config");
 const dbgr = require("debug")("development: mongoose");
+
+// Get MongoDB URI from environment variable in production, fallback to config
+const MONGODB_URI = process.env.MONGODB_URI || config.get("MONGODB_URI");
+
+if (!MONGODB_URI) {
+  console.error("FATAL ERROR: MONGODB_URI is not defined");
+  process.exit(1);
+}
+
 mongoose
-  // .connect(`${config.get("MONGODB_URI")}/SportSphere`)
-  .connect(`${config.get("MONGODB_URI")}`)
-  .then(() => dbgr("Connected to MongoDB..."))
-  .catch((err) => dbgr("Could not connect to MongoDB...", err));
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB...");
+    dbgr("Connected to MongoDB...");
+  })
+  .catch((err) => {
+    console.error("Could not connect to MongoDB...", err);
+    dbgr("Could not connect to MongoDB...", err);
+  });
 
 module.exports = mongoose.connection;
